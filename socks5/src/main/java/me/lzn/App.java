f6f23d412511d6428b06b45a707e1a99fd6a5d76
lzn <>
2023-01-07 10:45:48 +0800

    test 2

--- socks5/src/main/java/me/lzn/App.java
+++ socks5/src/main/java/me/lzn/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("ccc");
         System.out.println( "Hesdsdllo World!" );
+        System.out.println("test 2");
     }
 }
